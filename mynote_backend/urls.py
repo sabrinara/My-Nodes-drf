2c86cdf7de5a3e38ab7adaa59503e5da54a5ef61
TechBuilderTeam <saashid18@gmail.com>
2024-09-05 12:06:40 +0600

    frontend url added

--- mynote_backend/urls.py
+++ mynote_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from API.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
